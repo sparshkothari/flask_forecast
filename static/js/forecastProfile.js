2beb84c5bb3f732411efbdd5ef322c14b982af84
sparshkothari <sparshkothari@gmail.com>
2023-12-02 19:53:26 -0800

    feature_0.1.1 display charts for both merged and unmerged data

--- static/js/forecastProfile.js
+++ static/js/forecastProfile.js
@@ -48,11 +48,107 @@ var ForecastProfile = {
         }
     },
     chart: function (forecastObjDictArray) {
-
         am4core.useTheme(am4themes_animated);
 
-        var chart = am4core.create("forecastChartDiv", am4charts.XYChart);
+        let genericChartTitle = "Model: " + forecastObjDictArray[0]["forecast_base_model"]
+        let chartDisplayType = parseInt(document.getElementById("forecastDisplayType").value);
+        let chartDivElementIds = ForecastProfile.createChartDivElements(forecastObjDictArray.length)
+
+        let mergedData = ForecastProfile.spliceForecastArrayData(forecastObjDictArray)
+        let xAxisTitleText = "Day"
+        let yAxisTitleText = "Water (cubic inches)"
+        let chartMerged = ForecastProfile.createXYValueAxisChart(chartDivElementIds[0], mergedData, xAxisTitleText, yAxisTitleText)
+        let chartMergedTitle = chartMerged.titles.create()
+        chartMergedTitle.text = genericChartTitle
+        chartMerged.legend = new am4charts.Legend();
+
+        for (let [index, j] of forecastObjDictArray.entries()) {
+            let lineSeriesValueX = "day";
+            let lineSeriesValueY = j["forecast_data_key"]
+            let lineSeriesName = j["forecast_environment"]
+            ForecastProfile.createChartLineSeries(chartMerged, lineSeriesValueX, lineSeriesValueY, lineSeriesName, chartDisplayType)
+            if (forecastObjDictArray.length > 1) {
+                let jData = j["water_reserves_data"]
+                let jLineSeriesValueX = lineSeriesValueX
+                let jLineSeriesValueY = lineSeriesValueY
+                let jLineSeriesName = lineSeriesName
+                let chartJ = ForecastProfile.createXYValueAxisChart(chartDivElementIds[index + 1], jData, xAxisTitleText, yAxisTitleText)
+                ForecastProfile.createChartLineSeries(chartJ, jLineSeriesValueX, jLineSeriesValueY, jLineSeriesName, chartDisplayType)
+                let chartJTitle = chartJ.titles.create()
+                chartJTitle.text = genericChartTitle
+                chartJ.legend = new am4charts.Legend()
+            }
+        }
+    },
+    createXYValueAxisChart: function (divName, data, xAxisTitleText, yAxisTitleText) {
+        let chart = am4core.create(divName, am4charts.XYChart);
         chart.hiddenState.properties.opacity = 0; // this creates initial fade-in
+        let xAxis = chart.xAxes.push(new am4charts.ValueAxis());
+        let yAxis = chart.yAxes.push(new am4charts.ValueAxis());
+
+        chart.data = data
+        xAxis.title.text = xAxisTitleText;
+        yAxis.title.text = yAxisTitleText;
+        return chart;
+    },
+    createChartLineSeries: function (chart, lineSeriesValueX, lineSeriesValueY, lineSeriesName, chartDisplayType) {
+        var lineSeries = chart.series.push(new am4charts.LineSeries());
+        lineSeries.dataFields.valueX = lineSeriesValueX;
+        lineSeries.dataFields.valueY = lineSeriesValueY;
+        if (chartDisplayType === 0) {
+            lineSeries.fillOpacity = 0.3;
+            lineSeries.fill = am4core.color(ForecastProfile.colorKey.nextHexColor())
+            lineSeries.stroke = am4core.color(ForecastProfile.colorKey.nextHexColor())
+            lineSeries.strokeWidth = 1;
+        } else if (chartDisplayType === 1) {
+            lineSeries.stroke = am4core.color(ForecastProfile.colorKey.nextHexColor())
+            lineSeries.strokeWidth = 1;
+        } else if (chartDisplayType === 2) {
+            let nextHextColor = ForecastProfile.colorKey.nextHexColor()
+            lineSeries.fillOpacity = 0.3;
+            lineSeries.fill = am4core.color(nextHextColor)
+            lineSeries.stroke = am4core.color(nextHextColor)
+            lineSeries.strokeWidth = 1;
+        }
+        lineSeries.name = lineSeriesName;
+    },
+    createChartDivElements: function (length) {
+        let o = [];
+        let columnCount = 2;
+        let cellCount = length;
+
+        let divElements = document.getElementById("forecastChartDivRows");
+        divElements.innerHTML = "";
+
+        let parentChartDiv = document.createElement("div")
+        parentChartDiv.id = "parentForecastChartDiv"
+        parentChartDiv.style.height = "600px"
+        parentChartDiv.style.width = "100%"
+        divElements.appendChild(parentChartDiv)
+        o.push(parentChartDiv.id)
+        if (cellCount > 1) {
+            for (let i = 0; i < cellCount; i = i + columnCount) {
+
+                if (i % columnCount === 0) {
+                    let y = document.createElement("div");
+                    y.id = "dashDivRow" + i;
+                    y.classList = "w3-row";
+                    divElements.appendChild(y);
+
+                    for (let j = 0; j < columnCount; j++) {
+                        let u = document.createElement("div");
+                        u.classList = "w3-col s" + 12 / columnCount + " w3-center";
+                        u.id = "forecastChartDivCol" + (i + j);
+                        u.style.height = "600px"
+                        y.appendChild(u);
+                        o.push(u.id);
+                    }
+                }
+            }
+        }
+        return o;
+    },
+    spliceForecastArrayData: function (forecastObjDictArray) {
         let data = []
         let numberOfDays = parseInt(forecastObjDictArray[0]["duration_number_of_days"])
         for (let i = 0; i < numberOfDays; i++) {
@@ -64,30 +160,31 @@ var ForecastProfile = {
             }
             data.push(dataItem)
         }
-        chart.data = data
-
-        let xAxis = chart.xAxes.push(new am4charts.ValueAxis());
-        let yAxis = chart.yAxes.push(new am4charts.ValueAxis());
-
-        xAxis.title.text = "# of Days";
-        yAxis.title.text = "Water (cubic inches)";
-
-        for (let j of forecastObjDictArray) {
-            let forecast_data_key = j["forecast_data_key"]
-            let forecast_environment = j["forecast_environment"]
-            var waterSeries = chart.series.push(new am4charts.LineSeries());
-            waterSeries.dataFields.valueY = forecast_data_key;
-            waterSeries.dataFields.valueX = "day";
-            waterSeries.fillOpacity = 0.3;
-            waterSeries.fill = chart.colors.next()
-            waterSeries.stroke = chart.colors.next()
-            waterSeries.name = forecast_environment;
+        return data;
+    },
+    colorKey: {
+        key: [
+            {name: "Yellow", hexValue: "#FCE883"},
+            {name: "Lime", hexValue: "#00FF00"},
+            {name: "Purple", hexValue: "#800080"},
+            {name: "Magenta", hexValue: "#F664AF"},
+            {name: "Green", hexValue: "#1CAC78"},
+            {name: "Cyan", hexValue: "#00D7AF"},
+            {name: "Red", hexValue: "#EE204D"}
+        ],
+        counter: -1,
+        nextHexColor: function () {
+            if (this.counter === -1) {
+                this.counter = parseInt(Math.random() * this.key.length)
+            }
+            if (this.counter < (this.key.length - 1)) {
+                this.counter++;
+            } else {
+                this.counter = 0
+            }
+            return this.key[this.counter].hexValue
         }
-
-
-        chart.legend = new am4charts.Legend();
     }
-
 }
 
 ForecastProfile.init()
