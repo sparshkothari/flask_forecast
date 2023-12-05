e87f0ac826103e34727278a2b99567c966f6415e
sparshkothari <sparshkothari@gmail.com>
2023-12-05 10:23:46 -0800

    feature_0.2.1 adding new base model ("B" , "B1" and "B2")

---
+++ forecast/forecast_models/base_model_b.py
@@ -0,0 +1,91 @@
+# base_model_a.py
+import math
+from random import Random
+
+from forecast.forecast_models.forecast_model_template import ForecastModelTemplate, GenerateBaseModelArray, \
+    BaseModelChartVariables
+
+
+class GenerateBaseModelBArray(GenerateBaseModelArray):
+
+    def __init__(self, timeframe_multiplier: float, timeframe_unit: int, timeframe_increment_multiplier: float):
+        super().__init__(timeframe_multiplier, timeframe_unit, timeframe_increment_multiplier)
+        self.array.append(B1(timeframe_multiplier, timeframe_unit, timeframe_increment_multiplier))
+        self.array.append(B2(timeframe_multiplier, timeframe_unit, timeframe_increment_multiplier))
+
+
+class BaseModelBChartVariables(BaseModelChartVariables):
+
+    def __init__(self, timeframe_multiplier: float, timeframe_unit: int, timeframe_increment_multiplier: float):
+        super().__init__(timeframe_multiplier, timeframe_unit, timeframe_increment_multiplier)
+        self.title += "B"
+        self.yAxisTitleText = "Population (# of people)"
+
+
+class BaseModelB(ForecastModelTemplate):
+
+    def __init__(self, timeframe_multiplier: float, timeframe_unit: int, timeframe_increment_multiplier: float):
+        super().__init__(timeframe_multiplier, timeframe_unit, timeframe_increment_multiplier)
+        self.base_model = "B"
+
+        self.initial_data_point = math.pow(10, 5)
+        self.data_point = self.initial_data_point
+
+        self.birth_rate_daily = 0.0
+        self.death_rate_daily = 0.0
+
+        self.small_calamity_probability = 0.0
+        self.large_calamity_probability = 0.0
+
+        self.percentage_population_lost_small_calamity = 0.00
+        self.percentage_population_lost_large_calamity = 0.00
+
+    def iterate(self):
+        self.birth()
+        self.death()
+        self.small_calamity()
+        self.large_calamity()
+
+    def birth(self):
+        self.data_point += (self.birth_rate_daily *
+                            self.data_point)
+
+    def death(self):
+        self.data_point -= (self.death_rate_daily *
+                            self.data_point)
+
+    def small_calamity(self):
+        if Random().random() < self.small_calamity_probability:
+            self.data_point -= (self.percentage_population_lost_small_calamity *
+                                self.data_point)
+
+    def large_calamity(self):
+        if Random().random() < self.large_calamity_probability:
+            self.data_point -= (self.percentage_population_lost_large_calamity *
+                                self.data_point)
+
+
+class B1(BaseModelB):
+
+    def __init__(self, timeframe_multiplier: float, timeframe_unit: int, timeframe_increment_multiplier: float):
+        super().__init__(timeframe_multiplier, timeframe_unit, timeframe_increment_multiplier)
+
+        self.lineSeriesName = "sustainable—model " + self.__class__.__name__
+
+        self.birth_rate_daily = 0.0004
+        self.death_rate_daily = 0.0004
+        self.small_calamity_probability = 0.005
+        self.large_calamity_probability = 0.001
+
+
+class B2(BaseModelB):
+
+    def __init__(self, timeframe_multiplier: float, timeframe_unit: int, timeframe_increment_multiplier: float):
+        super().__init__(timeframe_multiplier, timeframe_unit, timeframe_increment_multiplier)
+
+        self.lineSeriesName = "sustainable—model " + self.__class__.__name__
+
+        self.birth_rate_daily = 0.000401
+        self.death_rate_daily = 0.0004
+        self.small_calamity_probability = 0.005
+        self.large_calamity_probability = 0.001
--- forecast/forecast_models/forecast_model_template.py
+++ forecast/forecast_models/forecast_model_template.py
@@ -1,40 +1,53 @@
 # forecast_model_template.py
+import math
 
 
 class GenerateBaseModelArray:
 
-    def __init__(self, forecast_timeframe: float):
+    def __init__(self, timeframe_multiplier: float, timeframe_unit: int, timeframe_increment_multiplier: float):
         self.array = []
         pass
 
 
 class BaseModelChartVariables:
 
-    def __init__(self, forecast_timeframe: float):
+    def __init__(self, timeframe_multiplier: float, timeframe_unit: int, timeframe_increment_multiplier: float):
         self.title = "Model: "
-        self.xAxisTitleText = ""
+        self.lineSeriesValueX = ForecastModelTemplate(-1, timeframe_unit, -1).lineSeriesValueX
+        self.xAxisTitleText = self.lineSeriesValueX
         self.yAxisTitleText = ""
-        self.lineSeriesValueX = ""
-        self.forecast_timeframe = ""
 
 
 class ForecastModelTemplate:
 
-    def __init__(self, forecast_timeframe: float):
+    def __init__(self, timeframe_multiplier: float, timeframe_unit: int, timeframe_increment_multiplier: float):
         self.base_model = ""
-        self.model = ""
-        self.forecast_timeframe = -1
+        self.model = self.__class__.__name__
         self.lineSeriesValueX = "value_x_"
-        self.lineSeriesValueY = "value_y_"
+        self.lineSeriesValueY = "value_y_" + self.model
         self.lineSeriesName = ""
+        self.timeframe = -1
+        self.timeframe_increment = -1
+        if timeframe_unit == 0:
+            self.timeframe = int(timeframe_multiplier * 365)
+            self.timeframe_increment = int(timeframe_increment_multiplier * 365)
+            self.lineSeriesValueX += "day"
+
+        if timeframe_increment_multiplier == 0.0:
+            self.timeframe_increment = 1.0
+
         self.data_point = 0.0
+        self.initial_data_point = 0.0
+        self.final_differential = 0.0
         self.data = []
 
     def simulate_model(self):
-        for i in range(0, self.forecast_timeframe):
+        for i in range(0, self.timeframe):
             self.iterate()
-            data_item = {self.lineSeriesValueX: i, self.lineSeriesValueY: self.data_point}
-            self.data.append(data_item)
+            if math.floor(i % self.timeframe_increment) == 0:
+                data_item = {self.lineSeriesValueX: i, self.lineSeriesValueY: self.data_point}
+                self.data.append(data_item)
+        self.final_differential = self.data_point - self.initial_data_point
 
     def iterate(self):
         pass
