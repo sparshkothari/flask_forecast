1cd8c8d3b0db62301218d8a1ac6371a0709df1c3
sparshkothari <sparshkothari@gmail.com>
2023-12-07 11:22:21 -1000

    feature-0.3.1 adding model C ("C6")

--- forecast/forecast_models/base_model_c.py
+++ forecast/forecast_models/base_model_c.py
@@ -16,6 +16,7 @@ class GenerateBaseModelCArray(GenerateBaseModelArray):
         self.array.append(C3(timeframe_multiplier, timeframe_unit, timeframe_increment_multiplier))
         self.array.append(C4(timeframe_multiplier, timeframe_unit, timeframe_increment_multiplier))
         self.array.append(C5(timeframe_multiplier, timeframe_unit, timeframe_increment_multiplier))
+        self.array.append(C6(timeframe_multiplier, timeframe_unit, timeframe_increment_multiplier))
 
 
 class BaseModelCChartVariables(BaseModelChartVariables):
@@ -39,13 +40,14 @@ class BaseModelC(ForecastModelTemplate):
         self.frequency = (2 * math.pi) / 365
         self.phase = 0.0
 
+        self.tan = False
         self.cosine = False
         self.sine = False
         self.custom = False
 
     def iterate(self, index):
         super().iterate(index)
-        if self.cosine and self.sine:
+        if self.tan:
             self.data_point = forecast_utils.tan_f(self.amplitude, self.frequency, self.phase, index)
         elif self.cosine:
             self.data_point = forecast_utils.cosine_f(self.amplitude, self.frequency, self.phase, index)
@@ -55,10 +57,19 @@ class BaseModelC(ForecastModelTemplate):
             self.data_point = self.custom_f(self.amplitude, self.frequency, self.phase, index)
 
     def custom_f(self, a, f, p, x):
-        pass
+        return self.data_point
 
 
-class C1(BaseModelC):
+class CTan(BaseModelC):
+
+    def __init__(self, timeframe_multiplier: float, timeframe_unit: int, timeframe_increment_multiplier: float):
+        super().__init__(timeframe_multiplier, timeframe_unit, timeframe_increment_multiplier)
+
+        self.lineSeriesName = "example—model-tan " + self.__class__.__name__
+        self.tan = True
+
+
+class CCosine(BaseModelC):
 
     def __init__(self, timeframe_multiplier: float, timeframe_unit: int, timeframe_increment_multiplier: float):
         super().__init__(timeframe_multiplier, timeframe_unit, timeframe_increment_multiplier)
@@ -67,7 +78,7 @@ class C1(BaseModelC):
         self.cosine = True
 
 
-class C2(BaseModelC):
+class CSine(BaseModelC):
 
     def __init__(self, timeframe_multiplier: float, timeframe_unit: int, timeframe_increment_multiplier: float):
         super().__init__(timeframe_multiplier, timeframe_unit, timeframe_increment_multiplier)
@@ -76,19 +87,38 @@ class C2(BaseModelC):
         self.sine = True
 
 
-class C3(BaseModelC):
+class CCustom(BaseModelC):
 
     def __init__(self, timeframe_multiplier: float, timeframe_unit: int, timeframe_increment_multiplier: float):
         super().__init__(timeframe_multiplier, timeframe_unit, timeframe_increment_multiplier)
 
         self.lineSeriesName = "example—model-custom " + self.__class__.__name__
         self.custom = True
+
+
+class C1(CCosine):
+
+    def __init__(self, timeframe_multiplier: float, timeframe_unit: int, timeframe_increment_multiplier: float):
+        super().__init__(timeframe_multiplier, timeframe_unit, timeframe_increment_multiplier)
+
+
+class C2(CSine):
+
+    def __init__(self, timeframe_multiplier: float, timeframe_unit: int, timeframe_increment_multiplier: float):
+        super().__init__(timeframe_multiplier, timeframe_unit, timeframe_increment_multiplier)
+
+
+class C3(CCustom):
+
+    def __init__(self, timeframe_multiplier: float, timeframe_unit: int, timeframe_increment_multiplier: float):
+        super().__init__(timeframe_multiplier, timeframe_unit, timeframe_increment_multiplier)
+
         self.multiplier = 0.5
 
     def custom_f(self, a, f, p, x):
-        super().custom_f(a, f, p, x)
-        y = forecast_utils.sine_f(a, f, p, x)
         m = self.multiplier
+
+        y = forecast_utils.sine_f(a, f, p, x)
         y = y * forecast_utils.cosine_f(m * a, m * f, m * p, m * x)
         return y
 
@@ -97,11 +127,32 @@ class C4(C3):
 
     def __init__(self, timeframe_multiplier: float, timeframe_unit: int, timeframe_increment_multiplier: float):
         super().__init__(timeframe_multiplier, timeframe_unit, timeframe_increment_multiplier)
-        self.multiplier = 2
+        self.multiplier = 2.0
 
 
 class C5(C3):
 
     def __init__(self, timeframe_multiplier: float, timeframe_unit: int, timeframe_increment_multiplier: float):
         super().__init__(timeframe_multiplier, timeframe_unit, timeframe_increment_multiplier)
-        self.multiplier = 5
+        self.multiplier = 5.0
+
+
+class C6(CCustom):
+
+    def __init__(self, timeframe_multiplier: float, timeframe_unit: int, timeframe_increment_multiplier: float):
+        super().__init__(timeframe_multiplier, timeframe_unit, timeframe_increment_multiplier)
+
+        self.multiplier_1 = 0.5
+        self.multiplier_2 = 2.0
+        self.multiplier_3 = 5.0
+
+    def custom_f(self, a, f, p, x):
+        m1 = self.multiplier_1
+        m2 = self.multiplier_2
+        m3 = self.multiplier_3
+
+        y = forecast_utils.sine_f(a, f, p, x)
+        y = y * forecast_utils.cosine_f(m1 * a, m1 * f, m1 * p, m1 * x)
+        y = y * forecast_utils.sine_f(m2 * a, m2 * f, m2 * p, m2 * x)
+        y = y * forecast_utils.cosine_f(m3 * a, m3 * f, m3 * p, m3 * x)
+        return y
